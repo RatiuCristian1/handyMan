93221a4ba17f3ecbac8b0cd2da2f041b5fb001b7
Cristian Ratiu <lafurnica2008@yahoo.com>
2023-11-09 20:51:52 +0100

    Styling the home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import Products from './assets/Products';
 
 export default function Home() {
   const images = ["intro1.jpg", "intro2.jpg", "intro3.jpg", "intro4.jpg", "intro5.jpg"];
-  const texts = ["Text for Image 1", "Text for Image 2", "Text for Image 3", "Text for Image 4", "Text for Image 5"];
+  const texts = ["Get your best power tools at HandyMan.com", "", "Text for Image 3", "Text for Image 4", "Text for Image 5"];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextImage = () => {
@@ -30,8 +30,41 @@ export default function Home() {
           <span className="material-symbols-outlined left--arrow">arrow_circle_right</span>
         </button>
       </div>
-      <div>
-      
+      <div className='home--products--container'>
+        <div className='home--angel--grinder--container'>
+          <div>
+            <p className='home--products--name'>Bosch AG50-10125 Angle Grinder</p>
+            <img className='home--products--images' src="Bosch AG50-10125 Angle Grinder.webp" alt="" />
+          </div>
+
+          <div>
+            <p className='home--products--name'>AEG-Angel-Grinder 230</p>
+            <img className='home--products--images' src="AEG-Angel-Grinder 230.webp" alt="AEG-Angel-Grinder" />
+          </div>
+
+          <div>
+            <p className='home--products--name'>DeWalt-Angel-Grinder125</p>
+            <img className='home--products--images' src="DeWalt-Angel-Grinder125.webp" alt="DeWalt-Angel-Grinder" />
+          </div>
+
+        </div>
+        <div className='home--power--drill--container'>
+          <div>
+            <p className='home--products--name'>Bosch600</p>
+            <img className='home--products--images' src="Bosch600.jpg" alt="" />
+          </div>
+
+          <div>
+            <p className='home--products--name'>Bosch1200</p>
+            <img className='home--products--images' src="Bosch1200.jpg" alt="" />
+          </div>
+
+          <div>
+            <p className='home--products--name'>Ryobi800</p>
+            <img className='home--products--images' src="Ryobi800.webp" alt="" />
+          </div>
+          
+        </div>
       </div>
     </>
   );
