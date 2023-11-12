ccd481159390e2e6b804394c6cba0abe9bbef207
Cristian Ratiu <lafurnica2008@yahoo.com>
2023-11-12 18:24:11 +0100

    Adding some new images to the home page and styling them

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import Products from './assets/Products';
 
 export default function Home() {
-  const images = ["intro1.jpg", "intro2.jpg", "intro3.jpg", "intro4.jpg", "intro5.jpg"];
-  const texts = ["Get your best power tools at HandyMan.com", "", "Text for Image 3", "Text for Image 4", "Text for Image 5"];
+  const images = ["intro1.jpg", "introC.webp", "intro3.jpg", "intro4.jpg", "intro5.jpg"];
+  const texts = ["Get your best power tools at HandyMan.com",];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextImage = () => {
@@ -38,17 +38,17 @@ export default function Home() {
         <div className='home--angel--grinder--container'>
 
           <div>
-            <p className='home--products--name'>Bosch AG50-10125 Angle Grinder</p>
+            <p className='home--products--name'>Bosch AG50 Angle Grinder</p>
             <img className='home--products--images' src="Bosch AG50-10125 Angle Grinder.webp" alt="" />
           </div>
 
           <div>
-            <p className='home--products--name'>AEG-Angel-Grinder 230</p>
+            <p className='home--products--name'>AEG Angel-Grinder 230</p>
             <img className='home--products--images' src="AEG-Angel-Grinder 230.webp" alt="AEG-Angel-Grinder" />
           </div>
 
           <div>
-            <p className='home--products--name'>DeWalt-Angel-Grinder125</p>
+            <p className='home--products--name'>DeWalt Angel-Grinder125</p>
             <img className='home--products--images' src="DeWalt-Angel-Grinder125.webp" alt="DeWalt-Angel-Grinder" />
           </div>
 
@@ -89,10 +89,32 @@ export default function Home() {
           
         </div>
 
+        <div className='home--hand--tools--container'>
+          <div>
+            <p className='home--products--name'>Bosch-cordless18</p>
+            <img className='home--products--images' src="screwdriver-set-stanley.jpg" alt="" />
+          </div>
+
+          <div>
+            <p className='home--products--name'>DeWalt-cordless20</p>
+            <img className='home--products--images' src="industrial-leather-hand-gloves.jpg" alt="" />
+          </div>
+
+          <div>
+            <p className='home--products--name'>Makita-cordless18 Pack</p>
+            <img className='home--products--images' src="public\pliers-stanley.jpg" alt="" />
+          </div>
+          
+        </div>
+
+
       </div>
       <div className='home--banner'>
         <img src="construction111.jpg" alt="" />
+        <img className='home--banner2' src="construction1.jpg" alt="" />
+        {/* <img className='' src="construction1.jpg" alt="" /> */}
       </div>
+      <div className='wrench--banner'></div>
     </>
   );
 }
