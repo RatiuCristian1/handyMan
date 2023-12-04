ba94384cad9029fea524fab6253e61ff10120b37
Cristian Ratiu <lafurnica2008@yahoo.com>
2023-12-04 13:58:34 +0100

    Adding the delivery partners logo to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,12 @@ export default function Footer() {
     return (
         <div className="footer--container">
             <h1 className="footer--text">Footer</h1>
+            <div className="footer--images--container">
+                <h3>Delivery Partners</h3>
+                <img className="footer--images" src="dhl1.png" alt="" />
+                <img className="footer--images" src="hermes.webp" alt="" />
+                <img className="footer--images" src="ups.png" alt="" />
+            </div>
             <h3 className="footer--social">Social media</h3>
         </div>
     )
